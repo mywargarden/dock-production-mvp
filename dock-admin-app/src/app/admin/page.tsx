f02830ef96d8f795b4e01d6db2e29d59d0d6f611
mywargarden <mywargarden@gmail.com>
2026-08-26 11:18:20 -0400

    Add persistent navigation to District Admin

--- dock-admin-app/src/app/admin/page.tsx
+++ dock-admin-app/src/app/admin/page.tsx
@@ -1,1 +1,15 @@
-export { default } from './page_admin_surgical_fix'
+import Link from 'next/link'
+import AdminPage from './page_admin_surgical_fix'
+
+export default function DistrictAdminRoute() {
+  return (
+    <>
+      <div className="roleNav">
+        <Link href="/">← Dock Home</Link>
+        <span>District Admin</span>
+        <Link href="/owner">Dock HQ</Link>
+      </div>
+      <AdminPage />
+    </>
+  )
+}
